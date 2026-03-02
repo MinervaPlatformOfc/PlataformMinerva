09b1e54b51b898b200feb69e95730351303b3b05
ErickNeves07 <barbosa.erick.neves@gmail.com>
2026-03-01 21:50:52 -0300

    adjustes removing } after merge

--- src/main/java/com/example/minerva/servlet/adm/crudStudent/ServletDeleteStudent.java
+++ src/main/java/com/example/minerva/servlet/adm/crudStudent/ServletDeleteStudent.java
@@ -25,8 +25,8 @@ public class ServletDeleteStudent extends HttpServlet {
 
         StudentDAO studentRepository = new StudentDAO();
 
-            request.setAttribute("msg", studentRepository.delete(id) ? "Estudante removido com sucesso":"Falha ao remover estudante");
-        }
+        request.setAttribute("msg", studentRepository.delete(id) ? "Estudante removido com sucesso":"Falha ao remover estudante");
+
         RechargeListener rechargeListener = new RechargeListener();
         rechargeListener.rechargeForStudent();
         request.getRequestDispatcher("/admin/ViewStudents").forward(request,response);
