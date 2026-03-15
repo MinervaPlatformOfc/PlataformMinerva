b00acb7dd74a62f27d04af66ec78883789a419bf
João0524 <joaopedro.s.capelli@gmail.com>
2026-03-15 00:37:34 -0300

    Deixar o Crud Responsive

--- src/main/webapp/js/subjects.js
+++ src/main/webapp/js/subjects.js
@@ -3,9 +3,8 @@
 const perfilAluno = document.querySelector('#aluno-perfil');
 
 perfilAluno.addEventListener('click', ()=>{
-    window.location.href = "/pags/aluno.html";
+    window.location.href = "/aluno/home.jsp";
 });
-
 
 // Lógica pra clicar na logo = voltar inicio
 const voltarInicioImg = document.querySelector('.logo-central');
@@ -13,3 +12,15 @@ const voltarInicioImg = document.querySelector('.logo-central');
 voltarInicioImg.addEventListener('click', ()=>{
     window.location.href = "../index.html";
 });
+
+// Lógica para trocar as cores
+const body = document.body
+let casa = document.querySelector('input[name = "houseName"]').value;
+
+    if (casa === "grifinoria") {
+        body.classList.add('grifinoria')
+    } else if (casa === "sonserina") {
+        body.classList.add('sonserina')
+    } else if (casa === "corvinal") {
+        body.classList.add('corvinal')
+    }
