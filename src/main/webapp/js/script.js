f4f237efb344f460178e40678c68e9eb05026e3c
João0524 <joaopedro.s.capelli@gmail.com>
2026-03-17 07:39:02 -0300

    feat: add landing page

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,33 @@
+// const alohomora = document.querySelector('#alohomora-link')
+const cadastrar = document.querySelector('#cadastrar')
+const entrar = document.querySelector('#entrar')
+const efeito = document.querySelector('#efeito')
+
+// alohomora.addEventListener("click", ()=>{
+//     cadastrar.classList.add('leviowwsaah')
+//     setTimeout(()=>{
+//         cadastrar.classList.remove('leviowwsaah')
+//     }, 14000)
+// });
+
+//lógica para alohomora
+cadastrar.addEventListener("click", (e)=>{
+
+    efeito.style.left = e.clientX + "px"
+    efeito.style.top = e.clientY + "px"
+
+    efeito.style.transition = "transform 5s ease-in-out"
+    efeito.style.transform = "scale(200)"
+
+    setTimeout(() =>{
+        window.location.href = "register.jsp"
+    }, 7000)
+})
+
+entrar.addEventListener(()=>{
+    window.location.href = "login.jsp"
+})
+
+
+
+
